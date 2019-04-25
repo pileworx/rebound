4b071e0d544e81b3a62f2faad1220a0db596e2ab
MARCUS LANGE <marcus.lange@gmail.com>
2019-04-25 10:18:36 -0500

    added support for query string

--- src/main/scala/io/pileworx/rebound/routes/ReboundRoutes.scala
+++ src/main/scala/io/pileworx/rebound/routes/ReboundRoutes.scala
@@ -1,32 +1,43 @@
 package io.pileworx.rebound.routes
 
 import akka.http.scaladsl.model.StatusCodes.BadRequest
-import akka.http.scaladsl.model.{HttpCharsets, HttpEntity, MediaType}
+import akka.http.scaladsl.model.{ContentType, HttpCharsets, HttpEntity, MediaType, Uri}
 import akka.http.scaladsl.server.Directives.{get, _}
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.PathDirectives.path
 import akka.http.scaladsl.server.directives.RouteDirectives.complete
 import io.pileworx.rebound.AkkaImplicits
+import io.pileworx.rebound.storage.DefineMockCmd
 import io.pileworx.rebound.storage.MockData._
 
 object ReboundRoutes extends AkkaImplicits {
+
   val routes: Route = path(RemainingPath) { rPath =>
     parameterMap { params: Map[String, String] =>
-      def paramString(param: (String, String)): String = s"""${param._1}=${param._2}"""
-      val paramSt = s"${params.map(paramString).mkString("&")}"
-      //TODO sort out prefix ? for query string
+      val key: String = getPath(rPath, params)
       get {
-        val sRPath = s"${rPath.toString()}?$paramSt"
-        if (verb(GET).contains(sRPath)) {
-          val mock = verb(GET)(sRPath)
-          val mtParts = mock.contentType.split("/")
-          val mt = MediaType.customWithOpenCharset(mtParts(0), mtParts(1))
-          val ct = mt withCharset HttpCharsets.`UTF-8`
-          complete(mock.status -> HttpEntity(ct, mock.response))
+        if (verb(GET).contains(key)) {
+          val mock = verb(GET)(key)
+          complete(mock.status -> HttpEntity(getContentType(mock), mock.response))
         } else {
           complete(BadRequest, """{"status":"BAD REQUEST", "message":"No mocked data found"}""")
         }
       }
     }
+  }
+
+  private def getPath(rPath: Uri.Path, params: Map[String, String]): String = {
+    def paramString(param: (String, String)): String = s"""${param._1}=${param._2}"""
+
+    if (params.nonEmpty)
+      s"${rPath.toString()}?${params.map(paramString).mkString("&")}"
+    else
+      rPath.toString()
+  }
+
+  private def getContentType(mock: DefineMockCmd): ContentType.WithCharset = {
+    val mtParts = mock.contentType.split("/")
+    val mt = MediaType.customWithOpenCharset(mtParts(0), mtParts(1))
+    mt withCharset HttpCharsets.`UTF-8`
   }
 }
